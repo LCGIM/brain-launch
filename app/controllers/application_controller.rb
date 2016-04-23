a5301b7849a5da6734edeb83b7dad90599314aaa
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2016-04-23 16:39:34 -0400

    css-1 - first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_theme
+
+  private
+
+  def set_theme
+    ActionController::Base.template_root = File.join(RAILS_ROOT, 'app/views', theme) if theme
+  end
+
+  def theme
+    session[:theme] = params[:theme] if params[:theme]
+    session[:theme]
+  end
 end
